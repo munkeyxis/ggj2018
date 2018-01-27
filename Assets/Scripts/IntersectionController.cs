c578ef74e26b1d9218599dc3cf7da90ef317ff41
Chris Stevens <munkeyxis@gmail.com>
2018-01-27 17:51:42 -0500

    add signal lights

--- Assets/Scripts/IntersectionController.cs
+++ Assets/Scripts/IntersectionController.cs
@@ -4,6 +4,7 @@ public class IntersectionController : MonoBehaviour {
 
 	public Transform left_target;
 	public Transform right_target;
+    public SignalLightsController signalLightsController;
 
     private Vector3 leftTargetPos;
     private Vector3 rightTargetPos;
@@ -16,6 +17,7 @@ public class IntersectionController : MonoBehaviour {
 		int going_right_num;
 		going_right_num = Random.Range(0,1);
 		going_right = going_right_num == 0 ? false : true;
+        signalLightsController.SetLightOn(going_right);
         leftTargetPos = left_target.position;
         rightTargetPos = right_target.position;
 	}
@@ -29,6 +31,7 @@ public class IntersectionController : MonoBehaviour {
     public void ToggleTarget()
     {
         going_right = !going_right;
+        signalLightsController.SetLightOn(going_right);
         Debug.Log("Going right? " + going_right);
     }
 
---
+++ Assets/Scripts/SignalLightsController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SignalLightsController : MonoBehaviour {
+    public SpriteRenderer[] leftSignalLights;
+    public SpriteRenderer[] rightSignalLights;
+    public Sprite inactiveLight;
+    public Sprite activeLight;
+
+    public void SetLightOn(bool rightLightsOn)
+    {
+        if (rightLightsOn)
+        {
+            foreach (SpriteRenderer renderer in rightSignalLights)
+            {
+                renderer.sprite = activeLight;
+            }
+
+            foreach (SpriteRenderer renderer in leftSignalLights)
+            {
+                renderer.sprite = inactiveLight;
+            }
+        }
+        else
+        {
+            foreach (SpriteRenderer renderer in rightSignalLights)
+            {
+                renderer.sprite = inactiveLight;
+            }
+
+            foreach (SpriteRenderer renderer in leftSignalLights)
+            {
+                renderer.sprite = activeLight;
+            }
+        }
+    }
+}
