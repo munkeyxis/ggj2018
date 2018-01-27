5315c523b40878346cf43445c589bf44a63be00c
Christopher Graf <cgraf062491@gmail.com>
2018-01-27 15:29:59 -0500

    Intersection started
    here ya go

---
+++ Assets/Scripts/IntersectionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionController : MonoBehaviour {
+
+	public int left_target_num;
+	public int right_target_num;
+
+	private bool going_right;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		int going_right_num;
+		going_right_num = Random.Range(0,1);
+		going_right = going_right_num == 0 ? false : true;
+	}
+	
+	public int GetNextTarget(){
+
+		return going_right ? right_target_num : left_target_num;
+	}
+
+}
