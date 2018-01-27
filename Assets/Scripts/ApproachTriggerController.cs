89998ec9404f53c7596fa2e856ded1bfe7de8d55
Chris Stevens <munkeyxis@gmail.com>
2018-01-27 16:16:51 -0500

    create ApproachTriggerController, assign switch button

---
+++ Assets/Scripts/ApproachTriggerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ApproachTriggerController : MonoBehaviour {
+
+    public IntersectionController intersectionController;
+    public Button switchButton;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        switchButton.gameObject.SetActive(true);
+        // tell button to talk to me
+        // start sound
+        // start lights
+
+    }
+}
