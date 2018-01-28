d149e2891a04bafd0649592e501a9946fb269806
Christopher Graf <cgraf062491@gmail.com>
2018-01-28 14:41:57 -0500

    Scene Menu
    Menu Scene

---
+++ Assets/Scripts/ClickLoad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ClickLoad : MonoBehaviour {
+
+
+	public void LoadScene(int lvl){
+
+		SceneManager.LoadScene(lvl);
+	}
+}
