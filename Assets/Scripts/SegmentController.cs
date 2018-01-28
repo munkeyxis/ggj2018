87d603ea0b86870ae15fcd676ddadab7fd863fd3
Chris Stevens <munkeyxis@gmail.com>
2018-01-28 13:15:24 -0500

    refactoring target options

--- Assets/Scripts/SegmentController.cs
+++ Assets/Scripts/SegmentController.cs
@@ -2,4 +2,5 @@
 
 public class SegmentController : MonoBehaviour {
     public GameObject endTarget;
+
 }
--- Assets/Scripts/TargetOptions.cs
+++ Assets/Scripts/TargetOptions.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 
 public class TargetOptions : MonoBehaviour {
-    public SegmentController nextSegment;
+    public TargetOptions nextTarget;
     public bool noTarget;
-    public bool isPlayer1;
 
     public GameObject GetNextSegmentTarget()
     {
-        return nextSegment.endTarget;
+        return nextTarget.gameObject;
     }
 	
 }
