10d8337c608ce84471a2924fa48d4841ad7aedda
Chris Stevens <munkeyxis@gmail.com>
2018-01-28 10:59:28 -0500

    create Managers and assign static trainManager

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Managers : MonoBehaviour {
+
+    public static TrainManager trainManager { get; private set; }
+
+    private void Awake()
+    {
+        trainManager = GetComponent<TrainManager>();
+    }
+}
