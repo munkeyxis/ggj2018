3cd9339d89136d8810cd40e7eb233c0d6c5f39e7
Chris Stevens <munkeyxis@gmail.com>
2018-01-28 20:44:48 -0500

    assign intersectionTarget automatically

--- Assets/Scripts/IntersectionController.cs
+++ Assets/Scripts/IntersectionController.cs
@@ -4,14 +4,14 @@ public class IntersectionController : MonoBehaviour
 {
     public TargetOptions left_target;
     public TargetOptions right_target;
-    public TargetOptions intersectionTarget;
     public SignalLightsController signalLightsController;
-
     private bool going_right;
+    private TargetOptions intersectionTarget;
 
     void Start()
     {
         int going_right_num;
+        intersectionTarget = GetComponentInChildren<TargetOptions>();
         going_right_num = Random.Range(0, 2);
         going_right = going_right_num == 0 ? false : true;
         signalLightsController.SetLightOn(going_right);
